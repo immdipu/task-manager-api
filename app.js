56d548fd323bf1f625c4bcd977ec26ab147d3032
Dipu <dipuchaurasiya91@gmail.com>
2023-03-16 22:22:48 +0545

    access-control added for post

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ dotenv.config({ path: "./config.env" });
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   next();
 });
 
